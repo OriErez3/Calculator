7c8cdf69df36e5f8c417e92034c6c621c6f99517
OriErez3 <orierez3@gmail.com>
2025-12-05 20:43:30 -0500

    Got the backend working with a very basic frontend.

--- code.js
+++ code.js
@@ -13,19 +13,66 @@ function operate(x, y, operator){
         return x/y;
 }
 }
-first_num = 0
-second_num = 0
-operation = ""
-const container = document.getElementById("container");
+let first_num = null;
+let second_num = null;
+let temp_num = null; 
+let ans = null;
+let operation = "";
+let next_num = false; 
+const container = document.getElementById("oppscontainer");
 const opps_list = ["+", "-", "*", "/"]
+const calc_container = document.getElementById("calc_container");
+const screen = document.getElementById("screen");
 for (let i = 0; i < 4; i++){
     const opps = document.createElement("button");
     opps.classList.add("opperator");
     opps.textContent = opps_list[i];
     opps.addEventListener("click", ()=>{
+        if (first_num != null && second_num != null){
+            ans = operate(first_num, second_num, operation);
+            screen.value = ans;
+            first_num = ans; 
+            second_num = null;
+            next_num = false;
+        }
         operation = opps_list[i]
+        next_num = true;
     });
     container.appendChild(opps);
 }
+for (let i = 0; i < 10; i++){
+    const nums = document.createElement("button");
+    nums.classList.add("numbers");
+    nums.textContent = i;
+    nums.addEventListener("click", ()=>{
+        if (first_num == null && next_num==false){
+            first_num = Number(String(i));
+            screen.value = first_num;
+        }
+        else if (first_num != null && next_num==false){
+            first_num = Number(String(first_num)+String(i));
+            screen.value = first_num;
+        }
+        else if (second_num == null && next_num==true){
+            second_num = Number(String(i));
+            screen.value = second_num;
+        }
+        else if (second_num != null && next_num==true){
+            second_num = Number(String(second_num)+String(i));
+            screen.value = second_num;
+        }
+       
+ } );
+    container.appendChild(nums);
+}
 
-
+const equals = document.createElement("button");
+equals.classList.add("numbers");
+equals.textContent = "=";
+equals.addEventListener("click", ()=>{
+    ans = operate(first_num, second_num, operation);
+    screen.value = ans;
+    first_num = ans;
+    second_num = null; 
+});
+container.appendChild(equals);
